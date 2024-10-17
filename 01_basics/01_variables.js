77fcadfed5efa65d73c587f62a98534b101d3345
Khizra-shahzad <khizrashahzad22@gmail.com>
2024-10-17 03:40:31 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+let accountName = "khizra shahzad"
+const countryName = "pakistan"
+var accountNumber = "234556"
+//const value doesn't change at any condition
+//var value can be changed..
+var accountNumber="787878"
+const Name="bangladesh"
+
+console.table([accountName,countryName,accountNumber]);
