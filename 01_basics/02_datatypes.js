f35e623feb2baa664bd9f5da613fabe95396fc1e
Khizra-shahzad <khizrashahzad22@gmail.com>
2024-10-17 04:15:33 +0000

    container config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as a newer version.
+
+//alert("3+3") we are using node, not browser this type of code unly run in browser bcz node can't accept this.
+//alert("hello") it's the roght synatx node and browser accept this.
+
+let name="khizra"; //string datatype.
+let country="pakistan";// string datatypes.
+let age=18;//number,integer datatype.
+let clockTime=true;//boolean datatype.
+let result;//undefined datatype.
+// null data type have a empty value.
+
+console.log(typeof result );//typeof data type tell us the type of data.
+
+//string=> ""
+//number=> 2 to power 53
+//boolean=> true/false
+//undefined => when we not define the value :e.g:let number= ;
+//null=> standalone value 
+//symbol=> unique
+
