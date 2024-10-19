41e9434d4eba65a63128154852dfb49ccad2d2ed
Khizra-shahzad <khizrashahzad22@gmail.com>
2024-10-19 19:26:13 +0000

    container config

---
+++ 01_basics/03_datatype.conversion.js
@@ -0,0 +1,20 @@
+// let score="33"
+// console.log( typeof score);
+
+// let inNumber=String(score)
+//console.log(typeof inNumber);
+
+//  let inNumber=Number(score)
+//  console.log(typeof inNumber);
+
+
+//"33" easily convert in =>33
+//"33abc" in not convert in numbers at its produce output is NaN
+//true convert in one; false convert in 0
+
+let isLoggedIn=0
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//Boolean is also a type conversion which can convert 1 into true and 0 into false.
+// when we pass a empty strings "" the output is false.
+//when we pass a string "khizra" it will produce true.
