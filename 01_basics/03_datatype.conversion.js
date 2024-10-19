c87611dbbef7e06bc09ce4948a391ae4d20ab0fc
Khizra-shahzad <khizrashahzad22@gmail.com>
2024-10-19 20:10:56 +0000

    container config

--- 01_basics/03_datatype.conversion.js
+++ 01_basics/03_datatype.conversion.js
@@ -1,11 +1,11 @@
-// let score="33"
+let score="33"
 // console.log( typeof score);
 
-// let inNumber=String(score)
+ let inNumber=String(score)
 //console.log(typeof inNumber);
 
-//  let inNumber=Number(score)
-//  console.log(typeof inNumber);
+  let onNumber=Number(score)
+//  console.log(typeof onNumber);
 
 
 //"33" easily convert in =>33
@@ -14,7 +14,55 @@
 
 let isLoggedIn=0
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+
 //Boolean is also a type conversion which can convert 1 into true and 0 into false.
 // when we pass a empty strings "" the output is false.
 //when we pass a string "khizra" it will produce true.
+
+
+
+
+
+
+
+
+// ************************************OPERATIONS************************************
+let value=3
+let negValue=-value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2="khizra"
+
+let str3=str1+str2
+//console.log(str3);
+
+console.log("1" + 2);
+console.log(1+ "2");
+console.log("1" + 2 +2);
+console.log(1+2+"2");
+
+
+console.log(+true);
+
+let num1,num2,num3
+
+num1=num2=num3=2 + 2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
