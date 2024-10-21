db25ecb47601a8e5ff9395a59ba0efd74527a967
Khizra-shahzad <khizrashahzad22@gmail.com>
2024-10-21 06:25:30 +0000

    container config

---
+++ 01_basics/04_datatype.comparison.js
@@ -0,0 +1,33 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+
+console.log(undefined>0);
+console.log(undefined>=0);
+console.log(undefined==0);
+// we can't use this data types it's create inconsistency..
+
+
+// === => it's check data types strictly
+
+console.log("2"=== 2);
+
+
+
+
+
+
+
+
+
+
